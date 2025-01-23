de6bcc6f4805a21e7818bf8eef4ddf9be1f9f8a8
elias sp <elias_spiridonidis@hotmail.com>
2025-01-23 21:19:59 +0200

    hotfix completed. try catch added for arithmetic exception

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -12,6 +12,11 @@ public class Main {
         return a-b;
     }
     public static int div(int a,int b) {
-        return a/b;
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.err.println("error. denominator must not be zero.");
+            throw e;
+        }
     }
 }
